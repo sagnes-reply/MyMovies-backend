d1e9697b748185d46c34acaa4e5f869e60751298
sagnes-reply <167292112+sagnes-reply@users.noreply.github.com>
2024-05-14 15:59:03 +0200

    F: added call to get tv shows

--- server/MyMoviesServer.py
+++ server/MyMoviesServer.py
@@ -191,7 +191,7 @@ def create_movie_response(raw_response, array_id):
         movie_dict['title'] = movie['title']
         movie_dict['id'] = movie['id']
         movie_dict['overview'] = movie['overview']
-        movie_dict['genres'] = convert_genre_id(movie['genre_ids'])
+        movie_dict['genres'] = (movie['genre_ids'])
         movie_dict['releaseDate'] = convert_date(movie['release_date'])
         movie_dict['poster'] = get_poster(movie['poster_path'])
         movie_dict['favourite'] = check_favourite(movie['id'], array_id)
@@ -212,8 +212,23 @@ def get_genres():
     genres_strings = []
     for genre in genres:
         genres_strings.append(genre['name'])
-    print(genres_strings)
+   
+    #get tv shows genres
+    url = "https://api.themoviedb.org/3/genre/tv/list?language=en"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZmJlNDlmZTQyZDE2MjRiOTliYWZkMzZhNTJmZjEwZiIsInN1YiI6IjY2MzBjNDM4NWIyZjQ3MDEyODAzYjhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bhhXiBgwizqz5K98QXxE984zx9fh6Am_aNKObE3wi4k"
+    }
+
+    response = requests.get(url, headers=headers)
+    genres = response.json()['genres']
+  
+    for genre in genres:
+        genres_strings.append(genre['name'])
+
     return genres_strings
+
 
 def convert_genre_id(ids):
     url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
@@ -283,6 +298,55 @@ def update_favourite_movies():
     except Error as err:
         print("error in updeting user favourite movies: ", err)
         return jsonify(False), 500
+
+
+#----------------------------------------------------- TV SHOWS ----------------------------------------------------------------------------
+@app.route("/get_tv_shows", methods = ["GET"])
+def get_tv_shows():
+    print("------- GET TV SHOWS ---------")
+    username = request.args.get("username")
+    lang = request.args.get("lang")
+    url = "https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language="+lang+"&page=1&sort_by=popularity.desc"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZmJlNDlmZTQyZDE2MjRiOTliYWZkMzZhNTJmZjEwZiIsInN1YiI6IjY2MzBjNDM4NWIyZjQ3MDEyODAzYjhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bhhXiBgwizqz5K98QXxE984zx9fh6Am_aNKObE3wi4k"
+    }
+    response = requests.get(url, headers=headers)
+    array_id = []#get_favourites(username)
+    shows = create_tv_shows_response(response.json()['results'], array_id)
+    # Serializza l'array di film in JSON
+    return jsonify(shows)
+
+def create_tv_shows_response(raw_response, array_id):
+    shows = []
+    for show in raw_response:
+        show_dict = dict()
+        show_dict['title'] = show['original_name']
+        show_dict['id'] = show['id']
+        show_dict['overview'] = show['overview']
+        show_dict['genres'] = (show['genre_ids'])
+        show_dict['poster'] = get_poster(show['poster_path'])
+        show_dict['favourite'] = False #check_favourite(movie['id'], array_id)
+        shows.append(show_dict)
+    return shows
+
+@app.route("/get_tv_shows_genres", methods = ["GET"])
+def get_tv_shows_genres():
+    url = "https://api.themoviedb.org/3/genre/tv/list?language=en"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZmJlNDlmZTQyZDE2MjRiOTliYWZkMzZhNTJmZjEwZiIsInN1YiI6IjY2MzBjNDM4NWIyZjQ3MDEyODAzYjhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bhhXiBgwizqz5K98QXxE984zx9fh6Am_aNKObE3wi4k"
+    }
+
+    response = requests.get(url, headers=headers)
+    genres = response.json()['genres']
+    genres_strings = []
+    for genre in genres:
+        genres_strings.append(genre['name'])
+    print(genres_strings)
+    return genres_strings
 
 app.run(host="0.0.0.0", port="5001")
 
